a02b4c77417d175b7dffbd3dc5ccefedb1c4c199
Krishil Rohit Sheth <97043368+krishilsheth404@users.noreply.github.com>
2024-06-19 21:50:29 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -12,11 +12,11 @@ const mongoose = require("mongoose");
 const sessions = require('express-session');
 const cookieParser = require('cookie-parser');
 
-const Razorpay = require('razorpay');
-var instance = new Razorpay({
-    key_id: 'YOUR_KEY_ID',
-    key_secret: 'YOUR_KEY_SECRET',
-  });
+// const Razorpay = require('razorpay');
+// var instance = new Razorpay({
+//     key_id: 'YOUR_KEY_ID',
+//     key_secret: 'YOUR_KEY_SECRET',
+//   });
 
 const { MongoClient } = require('mongodb');
 
